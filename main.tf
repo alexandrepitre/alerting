cd286dceea4053ef42799e890320acc7b74fb111
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-06-01 14:00:54 -0400

    fghfgfg

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "mon-firestore-document-deletes" {
   conditions = {
     "Firestore Instance - Document Deletes" = {
     condition_treshold = {
-      filter     = "resource.type = \"firestore_instance\" AND metric.type = \"firestore.googleapis.com/document/delete_count\""
+      filter     = "metric.type=\"firestore.googleapis.com/document/delete_count\" AND resource.type=\"firestore_instance\""
       duration   = "3600s"
       comparison = "COMPARISON_GT"
       threshold_value = "10"
