d54350cb197bf5bec1048f14070c09df2477cd28
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-05-31 23:15:49 -0400

    fggfdd

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ module "mon-firestore-document-deletes" {
 
   conditions = {
     "Firestore Instance - Document Deletes" = {
-    condition_absent = {
+    condition_treshold = {
       filter     = "resource.type = \"firestore_instance\" AND metric.type = \"firestore.googleapis.com/document/delete_count\""
       duration   = "3600s"
       comparison = "COMPARISON_GT"
@@ -59,7 +59,7 @@ module "mon-firestore-document-reads" {
 
   conditions = {
     "Firestore Instance - Document Reads" = {
-    condition_absent = {
+    condition_treshold = {
       filter     = "resource.type = \"firestore_instance\" AND metric.type = \"firestore.googleapis.com/document/read_count\""
       duration   = "3600s"
       comparison = "COMPARISON_GT"
@@ -88,11 +88,40 @@ module "mon-firestore-document-writes" {
 
   conditions = {
     "Firestore Instance - Document Writes" = {
-    condition_absent = {
+    condition_treshold = {
       filter     = "resource.type = \"firestore_instance\" AND metric.type = \"firestore.googleapis.com/document/write_count\""
       duration   = "3600s"
       comparison = "COMPARISON_GT"
       threshold_value = "25"
+      aggregations_enabled = "true"
+      aggregations_alignment_period = "300s"
+      aggregations_per_series_aligner = "ALIGN_MEAN"
+      trigger_enabled = true
+      trigger_count = 1
+   }
+  }
+ }
+}
+
+module "mon-firestore-connected-clients" {
+  source = "./modules/monitoring-alert-policy"
+  display_name = "MOBILITY|${var.env_alert}|firestore|connected_clients|warn|metric"
+  project_id = var.project_id
+  user_labels = {env = "${var.env}", purpose = "firestore_connected_clients"}
+  combiner = "OR"
+  enabled = true
+  notification_channels = [
+    google_monitoring_notification_channel.email.name,
+    google_monitoring_notification_channel.snow.name
+  ]
+
+  conditions = {
+    "Firestore Instance - Document Writes" = {
+    condition_treshold = {
+      filter     = "resource.type = \"firestore_instance\" AND metric.type = \"firestore.googleapis.com/network/active_connections\""
+      duration   = "3600s"
+      comparison = "COMPARISON_GT"
+      threshold_value = "5"
       aggregations_enabled = "true"
       aggregations_alignment_period = "300s"
       aggregations_per_series_aligner = "ALIGN_MEAN"
