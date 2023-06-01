2a7ea7853bbcc55a48b8e55c83355573b1e43fba
Alexandre Pitre <alexandre.pitre@gmai.com>
2023-06-01 17:24:53 -0400

    erddrgtgtr

--- main.tf
+++ main.tf
@@ -38,7 +38,6 @@ module "mon-firestore-document-deletes" {
       aggregations_enabled = "true"
       aggregations_alignment_period = "300s"
       aggregations_per_series_aligner = "ALIGN_MEAN"
-      aggregations_cross_series_reducer = "REDUCER_NONE"
       trigger_enabled = true
       trigger_count = 1
    }
